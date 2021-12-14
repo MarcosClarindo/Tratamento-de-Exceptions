1c6342756620c8f8296cecae5db9380f8b97f2c4
Marco Clarindo <marcos_clarindo@hotmail.com>
2021-12-14 17:10:53 -0300

    Tratamento de Exceptions

---
+++ model/entities/Reservation.java
@@ -0,0 +1,85 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import model.exceptions.DomainException;
+
+public class Reservation {
+	private Integer roomNumber;
+	private Date checkin;
+	private Date checkout;
+	
+	public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	public Reservation(Integer roomNumber, Date checkin, Date checkout)  {
+		if (!checkout.after(checkin)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
+		
+		this.roomNumber = roomNumber;
+		this.checkin = checkin;
+		this.checkout = checkout;
+	}
+
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+
+	public Date getCheckin() {
+		return checkin;
+	}
+
+
+	
+
+
+	public Date getCheckout() {
+		return checkout;
+	}
+
+
+	public long duration() {
+		long diff = checkout.getTime() - checkin.getTime();
+		
+		// convertendo milesegundo para dias
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+	
+	public void updateDates(Date checkIn, Date checkOut){
+		
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			throw new DomainException("Reservation dates for update must be future dates");
+		}
+		if (!checkOut.after(checkIn)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
+		
+		this.checkin = checkIn;
+		this.checkout = checkOut;
+		
+	}
+	
+	@Override 
+	public String toString() {
+		return "Room "
+				+ roomNumber
+				+ ", check-in: "
+				+ sdf.format(checkin)
+				+ ", check-out: "
+				+ sdf.format(checkout)
+				+ ", "
+				+ duration()
+				+ " nights ";
+	}
+	
+	
+}
